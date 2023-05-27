d40f21e6f7f22932cccffc13c0704bbd4befa8d8
Dharvi Singhal <121503506+Dharvis22@users.noreply.github.com>
2023-05-27 13:57:47 +0530

    Update process_image.c

--- src/process_image.c
+++ src/process_image.c
@@ -6,19 +6,22 @@
 
 float get_pixel(image im, int x, int y, int c)
 {
-    // TODO Fill this in
-    return 0;
+   int m=x+y*im.w+c*im.w*im.h;
+    return im.data[m];
+    
 }
 
 void set_pixel(image im, int x, int y, int c, float v)
 {
-    // TODO Fill this in
+     if(x<0||x>=im.w||y<0||y>=im.h||c<0||c>=im.c){
+        return;
+    }
 }
 
 image copy_image(image im)
 {
     image copy = make_image(im.w, im.h, im.c);
-    // TODO Fill this in
+    memcpy(copy.data,im.data,im.w*im.h*im.c*sizeof(float));
     return copy;
 }
 
@@ -26,18 +29,45 @@ image rgb_to_grayscale(image im)
 {
     assert(im.c == 3);
     image gray = make_image(im.w, im.h, 1);
-    // TODO Fill this in
+    for(int x=0;x<im.w;x++){
+        for(int y=0;y<im.h;y++){
+            float r=get_pixel(im,x,y,0);
+            float g=get_pixel(im,x,y,1);
+            float b=get_pixel(im,x,y,2);
+            float gray_value=0.299*r +0.587*g +0.114*b;
+            set_pixel(gray,x,y,0,gray_value);
+
+        }
+    }
     return gray;
 }
 
 void shift_image(image im, int c, float v)
 {
-    // TODO Fill this in
+   for(int x=0;x<im.w;x++){
+        for(int y=0;y<im.h;y++){
+            float n=get_pixel(im,x,y,c);
+            set_pixel(im,x,y,c,n+v);
+        }
+    }
 }
 
 void clamp_image(image im)
 {
-    // TODO Fill this in
+    for(int c=0;c<im.c;c++){
+    for(int x=0;x<im.w;x++){
+        for(int y=0;y<im.h;y++){
+            float k=get_pixel(im,x,y,c);
+            if(k<0){
+                set_pixel(im,x,y,c,0);
+            }
+            else if(k>1){
+                set_pixel(im,x,y,c,1);
+            }
+        }
+
+    }
+   }
 }
 
 
@@ -54,10 +84,61 @@ float three_way_min(float a, float b, float c)
 
 void rgb_to_hsv(image im)
 {
-    // TODO Fill this in
+    for(int x=0;x<im.w;x++){
+        for(int y=0;y<im.h;y++){
+            float r=get_pixel(im,x,y,0);
+            float g=get_pixel(im,x,y,1);
+            float b=get_pixel(im,x,y,2);
+            float V=three_way_max(r,g,b);
+            float m=three_way_min(r,g,b);
+            float C=V-m;
+            float H;
+            if(C==0){
+                H=0;
+            }
+            else if(V==r){
+                H=(g-b)/C;
+            }
+            else if(V==g){
+                H=((b-r)/C)+2;
+
+            }
+            else if(V==b){
+                H=((r-g)/C)+4;
+            }
+            float h;
+            if(H<0){
+                h=H/6+1;
+            }
+            else{
+                h=H/6;
+            }
+            float s;
+            if(V==0){
+                s=0;
+
+            }
+            else{
+                s=C/V;
+            }
+            set_pixel(im,x,y,0,h);
+            set_pixel(im,x,y,1,s);
+            set_pixel(im,x,y,2,V);
+
+
+        }
+    }
 }
 
 void hsv_to_rgb(image im)
 {
-    // TODO Fill this in
+    for(int x=0;x<im.w;x++){
+        for(int y=0;y<im.h;y++){
+            float h=get_pixel(im,x,y,0);
+            float s=get_pixel(im,x,y,1);
+            float V=get_pixel(im,x,y,2);
+            float c=s*V;
+
+        }
+    }
 }
